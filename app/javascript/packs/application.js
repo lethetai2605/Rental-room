3e3a784452cd7dc2d1761f5f962d3f646f21202b
Tus <pmtuss@gmail.com>
2022-05-13 17:35:46 +0700

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// require("@popperjs/core")
+
+import "bootstrap"
+import "../stylesheets/application.scss"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
